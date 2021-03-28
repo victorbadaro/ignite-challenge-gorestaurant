24d2cbbf7ea0557f3d112729c6573743420600e8
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-03-28 09:28:23 -0300

    chore: change Modal component
    It's a Function Component now

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,58 +1,37 @@
-import { Component } from 'react';
+// import { useState } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
+ReactModal.setAppElement('#root');
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
-
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
-
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
+export function Modal({ isOpen, setIsOpen, children }) {
+    // const [isModalOpen, setIsModalOpen] = useState(isOpen);
 
     return (
-      <ReactModal
-        shouldCloseOnOverlayClick={!false}
-        onRequestClose={setIsOpen}
-        isOpen={modalStatus}
-        ariaHideApp={false}
-        style={{
-          content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            background: '#F0F0F5',
-            color: '#000000',
-            borderRadius: '8px',
-            width: '736px',
-            border: 'none',
-          },
-          overlay: {
-            backgroundColor: '#121214e6',
-          },
-        }}
-      >
-        {children}
-      </ReactModal>
+        <ReactModal
+            shouldCloseOnOverlayClick={!false}
+            onRequestClose={setIsOpen}
+            isOpen={isOpen}
+            ariaHideApp={false}
+            style={{
+                content: {
+                    top: '50%',
+                    left: '50%',
+                    right: 'auto',
+                    bottom: 'auto',
+                    marginRight: '-50%',
+                    transform: 'translate(-50%, -50%)',
+                    background: '#F0F0F5',
+                    color: '#000000',
+                    borderRadius: '8px',
+                    width: '736px',
+                    border: 'none',
+                },
+                overlay: {
+                    backgroundColor: '#121214e6',
+                },
+            }}
+        >
+            {children}
+        </ReactModal>
     );
-  }
-};
-
-export default Modal;
+}
--- src/components/ModalAddFood/index.jsx
+++ src/components/ModalAddFood/index.jsx
@@ -2,7 +2,7 @@ import { createRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
-import Modal from '../Modal';
+import { Modal } from '../Modal';
 import Input from '../Input';
 
 export function ModalAddFood({ isOpen, setIsOpen, handleAddFood }) {
